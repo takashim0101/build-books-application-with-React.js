34405bc548de199814c46e947bb72f9f9f00fb67
Takashi Murachi <takashim@missionreadyhq.com>
2025-05-10 15:14:18 +1200

    adjusted search box and search button in Navbar as the same height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// 
-// const Navbar = () => {
-//   return (
-//     <div className="navbar">
-//       <div>
-//         <h1>
-//           Marketing Association NZ <span className="highlight">Books App</span>
-//         </h1>
-//       </div>
-// 
-//       <div>
-//         <Link to="/">Home</Link>
-// 
-//         <Link to="/favorites">Your Favorites</Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// 
-// const Navbar = () => {
-//   return (
-//     <div className="navbar">
-//       <header className="header">
-//         <div className="logo">My Company</div>
-//         <nav className="navbar">
-//           <ul>
-//             <li>Menu 1</li>
-//             <li>Menu 2</li>
-//             <li>Menu 3</li>
-//             <li><button className="login-button">Login</button></li>
-//           </ul>
-//         </nav>
-//       </header>
-//       <main className="main-content">
-//         <h1>Lorem ipsum dolor sit amet</h1>
-//         <div className="search-container">
-//           <input type="text" placeholder="Search..." />
-//           <button className="search-button">SEARCH</button>
-//         </div>
-//       </main>
-//     </div>
-//   );
-// };
-// 
-// export default Navbar;
-
-
 import React, { useState } from "react";
 import styles from "./Navbar.module.css"; 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
@@ -78,7 +24,7 @@ const Navbar = ({ onSearch }) => {
         />
         <button onClick={handleSearch} className={styles.searchButton}>
           <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />
-          <span className={styles.buttonText}>Search</span> {/* テキストを追加 */}
+          <span className={styles.buttonText}>Search</span> {/* add text */}
         </button>
       </div>
     </div>
