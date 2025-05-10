7bbe209418f858dd66182010e860ffbcb33ec8f6
Takashi Murachi <takashim@missionreadyhq.com>
2025-05-10 16:52:40 +1200

    add 2 animation png file in Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,20 +11,26 @@ const Navbar = ({ onSearch }) => {
   };
 
   return (
-    <div className={styles.appContainer}>
-      <h1>A room without books is like a body without a soul.</h1> {/* Added text */}
-      <h2>Book Finder</h2> {/* Book Finder title */}
-      <div className={styles.searchContainer}>
+    <div className={styles.appContainer}>      
+      <h1>A room without books is like a body without a soul.</h1>
+      <h2>Book Finder</h2>
+      
+      <div className={styles.headerImages}>
+        <img src="./images/bg1.png" alt="Background 1" />
+        <img src="./images/bg2.png" alt="Background 2" />
+      </div>
+      
+      <div className={styles.searchContainer}>        
         <input
           type="text"
           placeholder="Enter your Book Name"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)} // Update search term on input change
           className={styles.searchInput} // Apply styles
-        />
+        />        
         <button onClick={handleSearch} className={styles.searchButton}>
           <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />
-          <span className={styles.buttonText}>Search</span> {/* add text */}
+          <span className={styles.buttonText}>Search</span>
         </button>
       </div>
     </div>
@@ -32,6 +38,8 @@ const Navbar = ({ onSearch }) => {
 };
 
 export default Navbar;
+
+
 
 
 
